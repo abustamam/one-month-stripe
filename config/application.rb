46aa6aa323410ac57f03dea9ee74f20a55085c88
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2014-08-16 22:56:33 -0700

    change config file for precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module OneMonthStripePayments
 
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.serve_static_assets = true
+
+    config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
+    config.assets.compile = true
+
   end
 end
