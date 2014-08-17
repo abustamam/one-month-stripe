8390aee441bdc2a0754db0bc60610705c8f47211
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2014-08-16 22:58:36 -0700

    Change application.rb to fix assets in heroku

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,5 @@ module OneMonthStripePayments
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.serve_static_assets = true
 
-    config.action_dispatch.x_sendfile_header = ‘X-Accel-Redirect’
-    config.assets.compile = true
-
   end
 end
