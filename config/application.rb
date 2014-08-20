243f40fcc0bb4c215d38773cbe196261d7c8ca87
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2014-08-19 17:15:41 -0700

    Add custom buy button

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module OneMonthStripePayments
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.js)
     config.serve_static_assets = true
 
   end
